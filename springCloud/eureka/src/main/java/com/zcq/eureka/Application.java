7e897ed64e572c9cf794878dffd399e924004619
zcq <z.github@gmail.com>
2017-06-16 17:12:03 +0800

    调整目录结构

---
+++ springCloud/eureka/src/main/java/com/zcq/eureka/Application.java
@@ -0,0 +1,16 @@
+package com.zcq.eureka;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by zcq on 2017/6/13.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
